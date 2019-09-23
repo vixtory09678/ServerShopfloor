156e9c6009e09e4d1c729307f4534cf7d299d223
roboticsza <robotics.za@gmail.com>
2019-09-23 12:03:07 +0700

    edit debug print error

--- config/server_config.js
+++ config/server_config.js
@@ -3,12 +3,19 @@ exports.getServerConfig = function () {
         'host': '192.168.43.249',
         'port': '5002',
         'reconnectInterval': 1000,
-        'reconnectTimes': 100
+        'reconnectTimes': 100,
+        'autoReconnect': true,
+        'reconnectTimeout': 1000,
+        'timeout': 5000
     }, {
-        'host': '192.168.43.22',
+        'host': '192.168.43.1',
         'port': '5002',
         'reconnectInterval': 1000,
-        'reconnectTimes': 100
+        'reconnectTimes': 100,
+        'autoReconnect': true,
+        'reconnectTimeout': 1000,
+        'timeout': 5000,
+        'unitId': 2
     }]
 
 }
