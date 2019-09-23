ebadc0aee56a5032fd99c77e5184eac3ab8f16be
roboticsza <robotics.za@gmail.com>
2019-09-23 10:42:58 +0700

    add multi-connection

--- config/modbus.js
+++ config/modbus.js
@@ -2,37 +2,41 @@
 
 const modbus = require('jsmodbus')
 var reconnect = require('net-socket-reconnect')
+var serverConfig = require('./server_config')
 
-var options = {
-    'host': '192.168.43.249',
-    'port': '5002',
-    'reconnectInterval': 1000,
-    'reconnectTimes': 100
-}
+var options = serverConfig.getServerConfig()
+console.log(options);
 
-var socket = reconnect(options) // no change to original API
+var socket = []
+options.forEach(option => {
+    socket.push(reconnect(option))
+});
+
 var modbusController = require('../app/controllers/modbus.controller')
 
 exports.run = function (mqtt) {
-    var modbusClient = new modbus.client.TCP(socket)
-    socket.on('connect', function () {
+    for (let i = 0; i < socket.length; i++) {
+        var modbusClient = new modbus.client.TCP(socket[i])
+        socket[i].on('connect', function () {
 
-        setInterval(function () {
-            modbusController.requestControlChartData(modbusClient, socket, mqtt)
-        }, 4000)
-        // setInterval(function () {
-        //     modbusController.requestParetoChartData(modbusClient, socket, mqtt)
-        // }, 4000)
-        setInterval(function () {
-            modbusController.requestAlarm(modbusClient, socket, mqtt)
-        }, 1000)
-    });
-    socket.on('error', function (err) {
-        console.log('Client Error', err)
-    })
-    socket.on('close', function (err) {
-        console.log('Client Close', err)
-    })
+            setInterval(function () {
+                modbusController.requestControlChartData(modbusClient, socket[i], mqtt)
+            }, 4000)
+            // setInterval(function () {
+            //     modbusController.requestParetoChartData(modbusClient, socket[i], mqtt)
+            // }, 4000)
+            setInterval(function () {
+                modbusController.requestAlarm(modbusClient, socket[i], mqtt)
+            }, 1000)
+        });
+        socket[i].on('error', function (err) {
+            console.log('Client Error', err)
+        })
+        socket[i].on('close', function (err) {
+            console.log('Client Close', err)
+        })
+    }
+
 
     // socket.connect(options)
 }
---
+++ config/server_config.js
@@ -0,0 +1,16 @@
+exports.getServerConfig = function () {
+    return [{
+            'host': '192.168.43.249',
+            'port': '5002',
+            'reconnectInterval': 1000,
+            'reconnectTimes': 100
+        }
+        // , {
+        //     'host': '192.168.43.249',
+        //     'port': '5002',
+        //     'reconnectInterval': 1000,
+        //     'reconnectTimes': 100
+        // }
+    ]
+
+}
